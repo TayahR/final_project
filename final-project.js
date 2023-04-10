577f9bc5ece834804270229b2d0f3662ad18d5ef
TayahR <128653229+TayahR@users.noreply.github.com>
2023-04-10 18:02:37 +1000

    V2 Responsive

---
+++ final-project.js
@@ -0,0 +1,13 @@
+// looking to do a side menu for mobile option - similar to slack exercise
+
+//function sideMenuAppears(){
+//     document.getElementById('side-menu').classList.add('show-menu');
+// }
+
+// function sideMenuDisappears(){
+//     document.getElementById('side-menu').classList.remove('show-menu')
+// }
+
+// document.getElementById('menu-trigger').addEventListener('click', sideMenuAppears);
+
+// document.getElementById('menu-close').addEventListener('click', sideMenuDisappears);
