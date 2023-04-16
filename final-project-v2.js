f284a37b0ba981896011eec352c84bd7907a0c1f
TayahR <128653229+TayahR@users.noreply.github.com>
2023-04-16 18:11:09 +1000

    Added search JS and burger

--- final-project-v2.js
+++ final-project-v2.js
@@ -1,56 +1,37 @@
-//WIP - Dave to forward article for reference 
-// link to Job Search button
+// SEARCH BAR - searches through the list of positions available to narrow down the options
 
-const jobListings = [{
-    jobID: 0,
-    jobType: 'Manager',
-    jobName: 'Manager Planning and Building',
-    companyName: 'Hepburn Shire Council',
-}, {
-    jobID: 1,
-    jobType: 'Director',
-    jobName: 'Director Connected Communities',
-    companyName: 'City of Whittlesea',
-}, {
-    jobID: 2,
-    jobType: 'Manager',
-    jobName: 'Manager Finance',
-    companyName: 'City of Ballarat',
-}];
+const liElements = document.getElementsByClassName("position");
 
-const searchBar = document.getElementById('search');
+function search_position() {
+  
+const input = document.getElementById("searchbar").value;
+const cleanedInput = input.toLowerCase()
 
-searchBar.addEventListener('keyup', searchJobs);
-
-function searchJobs(e) {
-    const searchValue = e.target.value;
-        const result = [];
-        
-        for (let i = 0; i < jobListings.length; i++) {
-            if (searchValue == jobListings[i].jobName) {
-                result.push(jobListings[i].jobName)
-            }
+    for (let i = 0; i < liElements.length; i++) {
+        const liElement = liElements[i];
+        if (!liElement.textContent.toLowerCase().includes(cleanedInput)) {
+            liElement.style.display = "none";
+            console.log({ removed: liElements[i].textContent });
         }
-    return result;
+        else {
+            liElements[i].style.display="list-item";
+        }
+    }
 }
 
+//SIDE MENU for Mobile - hamburger menu
+
+function sideMenuAppears(){
+    document.getElementById('side-menu').classList.add('show-menu');
+}
+
+function sideMenuDisappears(){
+    document.getElementById('side-menu').classList.remove('show-menu')
+}
+document.getElementById('menu-trigger').addEventListener('click', sideMenuAppears);
+document.getElementById('menu-close').addEventListener('click', sideMenuDisappears);
 
 
 
 
-
-
-
-// was thinking to do a side menu for mobile option - similar to slack exercise
-
-//function sideMenuAppears(){
-//     document.getElementById('side-menu').classList.add('show-menu');
-// }
-
-// function sideMenuDisappears(){
-//     document.getElementById('side-menu').classList.remove('show-menu')
-// }
-
-// document.getElementById('menu-trigger').addEventListener('click', sideMenuAppears);
-
-// document.getElementById('menu-close').addEventListener('click', sideMenuDisappears);
+console.log("here")
